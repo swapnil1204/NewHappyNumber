cf7ff15058bf4d57cc4192134808e2bd7faba68f
swapnilvijaykumarbamb <swapnilbamb1204@gmail.com>
2019-10-06 06:56:54 +0530

    [ swapnil ] Refactor. remove unnecessary variable

--- src/main/java/com/thoughtworks/training/HappyNumber.java
+++ src/main/java/com/thoughtworks/training/HappyNumber.java
@@ -14,32 +14,32 @@ public class HappyNumber {
     public boolean check() {
         if (number == 1 || number == 10)
             return true;
-        List<Integer> digit = getdigitsList(number); //82 --> [2,8]
-        int sumOfSquare = getSumOfSquare(digit);  // [2,8] --> 68
+        List<Integer> digit = getdigitsList(number);
+        int sumOfSquare = getSumOfSquare(digit);
         if (sumOfSquare == 1) {
             return true;
         }
-        this.number = sumOfSquare; //68
-        List<Integer> secondIterationList = getdigitsList(number); //68 -->[8,6]
-        int sumOfSquare1 = getSumOfSquare(secondIterationList); //[8,6] --> 100
-        if (sumOfSquare1 == 1) {
+        this.number = sumOfSquare;
+        digit = getdigitsList(number);
+        sumOfSquare = getSumOfSquare(digit);
+        if (sumOfSquare == 1) {
             return true;
         }
-        this.number = sumOfSquare1; //100
-        List<Integer> thirdIterationList = getdigitsList(number); //100 -->[0,0,1]
-        int sumOfSquare3 = getSumOfSquare(thirdIterationList); //[0,0,1]--> 1
-        if (sumOfSquare3 == 1) {
+        this.number = sumOfSquare;
+        digit = getdigitsList(number);
+        sumOfSquare = getSumOfSquare(digit);
+        if (sumOfSquare == 1) {
             return true;
         }
         return false;
     }
 
-    private List<Integer> getdigitsList(int number) { //86
+    private List<Integer> getdigitsList(int number) {
         List<Integer> digit = new ArrayList<>();
-        while (number > 0) {    //86  8
-            int lastDigit = number % 10; //6  8
-            number = number / 10;  //8   0
-            digit.add(lastDigit);  //6  8
+        while (number > 0) {
+            int lastDigit = number % 10;
+            number = number / 10;
+            digit.add(lastDigit);
         }
         return digit;
     }
@@ -47,9 +47,9 @@ public class HappyNumber {
     private int getSumOfSquare(List<Integer> digit) {
         int sumOfSquare = 0;
         for (int i = 0; i < digit.size(); i++) {
-            sumOfSquare = sumOfSquare + digit.get(i) * digit.get(i); //36 + 64
+            sumOfSquare = sumOfSquare + digit.get(i) * digit.get(i);
         }
-        return sumOfSquare; //100
+        return sumOfSquare;
     }
 
     public static void main(String[] args) {
